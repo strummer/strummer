13fc60ba754e8e759537266920c87b7a3556c1e4
bdickason <dickason@gmail.com>
2013-07-22 23:30:25 -0400

    Added SCSS/Compass config file

---
+++ examples/product-page/config.rb
@@ -0,0 +1,7 @@
+### config.rb - Controls SASS/SCSS and Compass Settings ###
+
+http_path = "/"
+css_dir = "/static/css"
+sass_dir = "sass"
+images_dir = "/static/images"
+javascripts_dir = "/static/js"
