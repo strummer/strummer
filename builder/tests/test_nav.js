e07046c5c312e7f0103165c3e67f88de506e0efd
bdickason <dickason@gmail.com>
2013-09-09 15:50:46 -0400

    Added test to verify that panes are added properly

--- builder/tests/test_nav.js
+++ builder/tests/test_nav.js
@@ -31,27 +31,46 @@ describe('nav', function() {
     beforeEach(inject(function($controller) {
       ctrl = $controller(menuController, {$scope: scope, $element: null});
     }));
+
+    it('Should add panes', inject(function($controller, $rootScope) {
+      var pane1 = {}, pane2 = {};
+
+      // There should be no panes when we start
+      expect(scope.panes).toEqual([]);
+
+      // Add two panes
+      ctrl.addPane(pane1);
+      ctrl.addPane(pane2);
+
+      // There should be 2 panes when we finish
+      expect(scope.panes.length).toEqual(2);
+    }));
     
     it('Should start with no panes selected', inject(function($controller, $rootScope) {
       // Test impact on DOM
       var test = element.find('.active'); // We indicate an active pane by setting a class .active
-      var result = {};
+      var result = {};  // There should be no active panes
+
+      console.log(element);
 
       expect(JSON.stringify(test)).toEqual(JSON.stringify(result));
 
-      // Test actual Contoller
+      // Test actual Contoller with stub data
+      var pane1 = {}, pane2 = {};
+
       
-      expect(scope.panes).toEqual([]);
+
+      // 
 
     }));
-    it('Should select a single pane if no panes are selected', function() {
-      expect('a').toEqual('a');
+    it('Should select a single pane when clicked if no panes are selected', function() {
+      
     });
-    it('Should de-select the current pane if the current pane is selected', function() {
+    it('Should de-select the current pane when clicked if the current pane is selected', function() {
 
     });
 
-    it('Should select the current pane and de-select the other pane if another pane is selected', function() {
+    it('Should select the current pane when clicked and de-select the other pane if another pane is selected', function() {
 
     });
 
