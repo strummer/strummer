0d1d202761cc350345e66cc5c75d577b9092e046
bdickason <dickason@gmail.com>
2013-09-15 13:41:18 -0400

    Added tests for domain factory

--- builder/static/nav/settings.spec.js
+++ builder/static/nav/settings.spec.js
@@ -35,21 +35,50 @@ describe("directive: settings", function() {
         expect(scope.domain).toBeDefined();
     }));
     
-    it("Should display an empty domain by default", inject(function($controller, $rootScope) {
+    it("Should set the domain in the datastore", inject(function($controller, $rootScope) {
         expect(scope.domain).toEqual('');
     }));
 
     it("Should display the user's domain if a domain exists", inject(function($controller, $rootScope) {
+
     }));
 
     it("Should save the user's domain when the user clicks 'Save'", inject(function($controller, $rootScope) {
+
     }));
 
     it("Should display an error if the domain cannot be saved", inject(function($controller, $rootScope) {
+        
     }));
 
 });
 
 describe("factory: domainFactory", function() {
 
+    beforeEach(function() {
+        inject(function($injector) {
+            domainFactory = $injector.get('domainFactory'); // Get an instance of domainFactory we can test
+        });
+    });
+
+    it("Should have a variable to reference domains", inject(function() {
+        var domain = domainFactory.get();
+        expect(domain).toBeDefined();
+    }));
+
+    it("Should return an empty domain by default", inject(function() {
+        var expectedDomain = "";
+        var domain = domainFactory.get();
+
+        expect(domain).toEqual(expectedDomain);
+    }));
+
+    it("Should set the domain when a string is added", inject(function($controller, $rootScope) {
+        var expectedDomain = "strummer.io";
+        domainFactory.set(expectedDomain);
+
+        var domain = domainFactory.get();
+
+        expect(domain).toEqual(expectedDomain);
+    }));
 });
